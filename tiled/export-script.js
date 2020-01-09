197c98759e88addace64f860b9d419b1418eb4c6
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-01-09 23:57:58 +0100

    Work on tiled extension
    Custom JS export script.

--- tiled/export-script.js
+++ tiled/export-script.js
@@ -1,58 +1,141 @@
 (() => {
-    const SETTINGS = {
-        shortName: "deathfloor-export",
-        longName:  "deathfloor export script",
+    function readMap(fileName) {
+        console.log("READ MAP: " + fileName);
+        return new TileMap();
+    };
+
+    function writeMap(map, fileName) {
+        const errors = [];
+
+        const outputMap = {
+            level: {
+                size: {
+                    w: map.width,
+                    h: map.height,
+                },
+                tile_size: {
+                    w: map.tileWidth,
+                    h: map.tileHeight,
+                }
+            },
+            tiles: [],
+            objects: [],
+        };
+
+        for (let layerIdx = 0; layerIdx < map.layerCount; layerIdx++) {
+            let layer = map.layerAt(layerIdx);
+            if (layer.isTileLayer) {
+                outputMap.tiles = [].concat(
+                    outputMap.tiles,
+                    getTilesFromLayer(layer)
+                ); // [...outputMap.tiles, ...getTilesFromLayer(layer)]
+
+            } else if (layer.isObjectLayer) {
+                outputMap.objects = [].concat(
+                    outputMap.objects,
+                    getObjectsFromLayer(layer)
+                ); // [...outputMap.tiles, ...getTilesFromLayer(layer)]
+            }
+        }
+
+        // TODO save to file
+        const file = new TextFile(fileName, TextFile.WriteOnly);
+        file.write(JSON.stringify(outputMap));
+        file.commit();
+        file.close();
+
+        return errors.join(", ");
+    };
+
+    function getTilesFromLayer(layer) {
+        const output = [];
+        const layerSize = {
+            w: layer.size.width,
+            h: layer.size.height
+        };
+        const layerProps = layer.properties();
+        const tilesetsToAdd = {};
+
+        for (let y = 0; y < layerSize.h; y++) {
+            for (let x = 0; x < layerSize.w; x++) {
+                const tile = layer.tileAt(x, y);
+                if (tile) {
+                    const tileset = tile.tileset;
+                    const tilesetName = tileset.image.split("/").pop()
+                        || "MISSING-TILESET.png";
+                    tilesetsToAdd[tilesetName] = tileset;
+
+                    const tileProps = tile.properties();
+                    const tileOutput = {
+                        id: tile.id,
+                        ts: tilesetName,
+                        pos: {
+                            x: x * tile.size.width,
+                            y: y * tile.size.height,
+                        },
+                        props: Object.assign({}, layerProps, tileProps), // { ...layerProps, ...tileProps }
+                    };
+                    output.push(tileOutput);
+                }
+            }
+        }
+
+        createTilesetRonsFor(Object.values(tilesetsToAdd));
+
+        return output;
+    }
+
+    function createTilesetRonsFor(tilesets) {
+        console.log("TODO create RONs");
+        // for (let key in tilesets[0]) {
+        //     const val = tilesets[0][key];
+        //     console.log(`${key} : ${val}`);
+        // }
+    }
+
+    function getObjectsFromLayer(layer) {
+        const output = [];
+
+        const layerProps = layer.properties();
+
+        for (let object of layer.objects) {
+            const objectProps = object.properties();
+            const objectOutput = {
+                type: object.type,
+                pos: {
+                    x: object.x,
+                    y: object.y,
+                },
+                size: {
+                    w: object.width,
+                    h: object.height,
+                },
+                props: Object.assign({}, layerProps, objectProps), // { ...layerProps, ...tileProps }
+            };
+            output.push(objectOutput);
+        }
+
+        return output;
+    }
+
+    function outputFiles(_map, fileName) {
+        return [fileName];
     };
 
     function registerMapFormat() {
-        const readMap = fileName => {
-            console.log("READ MAP: " + fileName);
-            return new TileMap();
+        const NAME = {
+            shortName: "deathfloor-export",
+            longName:  "deathfloor export script",
         };
-
-        const writeMap = (map, fileName) => {
-            const errors = [];
-
-            const levelSize = {
-                w: map.width,
-                h: map.height,
-            };
-            const tileSize = {
-                w: map.tileWidth,
-                h: map.tileHeight,
-            };
-
-            for (let layerIdx = 0; layerIdx < map.layerCount; layerIdx++) {
-                let layer = map.layerAt(layerIdx);
-                if (layer.isTileLayer) {
-                    // TODO do tile layer stuff
-                } else if (layer.isObjectLayer) {
-                    // TODO do object layer stuff
-                }
-            }
-
-            // TODO save to file
-
-            return errors.join(", ");
-        };
-
-        const outputFiles = (map, fileName) => {
-            return [fileName];
-        };
-
         const mapFormat = {
-            name:        SETTINGS.longName,
+            name:        NAME.longName,
             extension:   "json",
             read:        readMap,
             write:       writeMap,
             outputFiles: outputFiles,
         };
-        tiled.registerMapFormat(SETTINGS.shortName, mapFormat);
+        tiled.registerMapFormat(NAME.shortName, mapFormat);
     }
 
-    function main() {
-        registerMapFormat();
-    }
-
-    main();
+    registerMapFormat();
 })();
---
+++ tiled/tileset.tsx
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tileset" tilewidth="16" tileheight="16" tilecount="96" columns="16">
+ <image source="tilesets/tileset.png" width="256" height="96"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="87">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
