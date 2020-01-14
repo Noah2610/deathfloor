53bb4a5a4ce6744fb61dd904a9ec15820e886020
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-01-14 16:30:31 +0100

    Rename to sprongen, adjust help messages, add README

--- spritesheet-ron-gen/src/opts.rs
+++ spritesheet-ron-gen/src/opts.rs
@@ -7,9 +7,11 @@ use structopt::StructOpt;
 #[structopt(
     name = meta::NAME,
     version = meta::VERSION,
+    about = meta::DESCRIPTION,
 )]
 pub struct Opts {
     /// Enable verbose logging.
+    ///
     /// Prints information about used options,
     /// what PNG files are read,
     /// and what RON files are being generated.
@@ -17,14 +19,15 @@ pub struct Opts {
     #[structopt(short, long)]
     pub verbose: bool,
 
-    /// Pretty format the generated RON files,
-    /// when this option is set.
-    /// Without this, generated RONs will have no new-lines/spacing.
+    /// Pretty format the generated RON files.
+    ///
+    /// Without this, generated RON files will have no new-lines/spacing.
     #[structopt(short, long)]
     pub pretty: bool,
 
     /// Use the given tile size.
-    /// SIZE format is `<width>x<height>`,
+    ///
+    /// <tile-size> format is `<width>x<height>`,
     /// where <width> and <height> are positive integers.
     #[structopt(short = "s", long, default_value = "32x32")]
     pub tile_size: Size,
