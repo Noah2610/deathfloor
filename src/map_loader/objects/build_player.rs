b45f8e5f34928d43a227d85b89ea95657b9c8f14
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-01-16 13:53:29 +0100

    Cleanup, remove warnings

--- src/map_loader/objects/build_player.rs
+++ src/map_loader/objects/build_player.rs
@@ -14,3 +14,46 @@ pub(super) fn build(
 
     Ok(entity)
 }
+
+// TODO
+// fn create_player(world: &mut World) -> Entity {
+//     const PLAYER_Z: f32 = 1.0;
+//     const PLAYER_SIZE: (f32, f32) = (32.0, 64.0);
+
+//     let player_settings = world.read_resource::<SettingsRes>().0.player.clone();
+
+//     let mut transform = Transform::default();
+//     transform.set_translation_xyz(0.0, 0.0, PLAYER_Z);
+
+//     let size = Size::from(PLAYER_SIZE);
+
+//     let sprite_render = {
+//         let mut spritesheet_handles =
+//             world.write_resource::<SpriteSheetHandles>();
+//         SpriteRender {
+//             sprite_number: 1,
+//             sprite_sheet:  spritesheet_handles
+//                 .get_or_load(resource("spritesheets/player.png"), world),
+//         }
+//     };
+
+//     let movement_data = player_settings.movement;
+
+//     let decr_velocity = DecreaseVelocity::from(movement_data.decr_velocity);
+
+//     // TODO GRAVITY
+//     // let gravity = Gravity::from(movement_data.gravity);
+
+//     world
+//         .create_entity()
+//         .with(Player::default())
+//         .with(transform)
+//         .with(Velocity::default())
+//         .with(size)
+//         .with(sprite_render)
+//         .with(ScaleOnce::default())
+//         .with(decr_velocity)
+//         // .with(gravity) // TODO
+//         .with(movement_data)
+//         .build()
+// }
--- src/map_loader/objects/mod.rs
+++ src/map_loader/objects/mod.rs
@@ -4,11 +4,9 @@ mod object_type;
 
 use super::map_data::prelude::*;
 use crate::components::prelude::*;
-use crate::helpers::resource;
-use amethyst::ecs::{EntityBuilder, World, WorldExt};
+use amethyst::ecs::{World, WorldExt};
 use amethyst::prelude::Builder;
 use deathframe::amethyst;
-use deathframe::handles::SpriteSheetHandles;
 use object_type::ObjectType;
 use std::convert::TryFrom;
 
@@ -16,14 +14,7 @@ pub(super) fn load_objects(
     world: &mut World,
     objects: &ObjectsData,
 ) -> amethyst::Result<()> {
-    const DEFAULT_Z: f32 = 0.1;
-
     for object in objects {
-        let mut transform: Transform = object.pos.into();
-        transform.set_translation_z(object.z_or(DEFAULT_Z));
-
-        let size: Size = object.size.into();
-
         let object_type = ObjectType::try_from(object.object_type.as_str())?;
 
         match object_type {
