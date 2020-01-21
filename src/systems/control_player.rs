53b9e574ac444723c1ebc046d59d477ac5550823
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-01-21 15:11:04 +0100

    Cleanup ControlPlayerSystem

--- src/systems/control_player.rs
+++ src/systems/control_player.rs
@@ -6,40 +6,18 @@ pub struct ControlPlayerSystem;
 
 impl<'a> System<'a> for ControlPlayerSystem {
     type SystemData = (
-        Read<'a, Time>,
         Read<'a, InputManager<IngameBindings>>,
         ReadStorage<'a, Player>,
-        // ReadStorage<'a, PhysicsData>,
         WriteRigidBodies<'a>,
-        // WriteStorage<'a, Velocity>,
-        // WriteStorage<'a, DecreaseVelocity>,
     );
 
     fn run(
         &mut self,
-        (
-            time,
-            input_manager,
-            players,
-            // physics_data_store,
-            mut rigid_bodies,
-            // mut velocities,
-            // mut decr_velocities,
-        ): Self::SystemData,
+        (input_manager, players, mut rigid_bodies): Self::SystemData,
     ) {
-        let dt = time.delta_seconds() as f32;
-
-        for (_, /*player_physics_data,*/ player_rigid_body) in
-            (&players, /*&physics_data_store,*/ &mut rigid_bodies).join()
-        {
+        for (_, player_rigid_body) in (&players, &mut rigid_bodies).join() {
             Axis::for_each(|axis| {
-                handle_move_on_axis(
-                    axis,
-                    dt,
-                    &input_manager,
-                    // player_physics_data,
-                    player_rigid_body,
-                );
+                handle_move_on_axis(axis, &input_manager, player_rigid_body);
             });
         }
     }
@@ -47,30 +25,10 @@ impl<'a> System<'a> for ControlPlayerSystem {
 
 fn handle_move_on_axis(
     axis: Axis,
-    dt: f32,
     input_manager: &InputManager<IngameBindings>,
-    // physics_data: &PhysicsData,
     rigid_body: &mut RigidBody,
-    // velocity: &mut Velocity,
-    // decr_velocity: &mut DecreaseVelocity,
 ) {
-    // let is_velocity_below_max = |rigid: &RigidBody| {
-    //     let (velocity, max_velocity_opt) = {
-    //         let vel = rigid.velocity().as_vector();
-    //         match &axis {
-    //             Axis::X => (vel.x, physics_data.max_velocity.0),
-    //             Axis::Y => (vel.y, physics_data.max_velocity.1),
-    //         }
-    //     };
-    //     max_velocity_opt
-    //         .map(|max_vel| velocity.abs() < max_vel)
-    //         .unwrap_or(true)
-    // };
-    // let is_velocity_above_max =
-    //     |rigid: &RigidBody| !is_velocity_below_max(rigid);
-
     let axis_binding = IngameAxisBinding::from(&axis);
-    // let center_of_mass = rigid_body.center_of_mass();
     if let Some(val) = input_manager.axis_value(axis_binding) {
         if val != 0.0 {
             let acceleration_opt = {
@@ -90,15 +48,11 @@ fn handle_move_on_axis(
             if let Some(acceleration) = acceleration_opt {
                 let speed = acceleration * val; // * dt; // NOTE: dt is included with some ForceTypes / physics bundle
 
-                // let should_apply_force = is_velocity_below_max(rigid_body);
-
-                // Apply force if below max velocity
-                // if should_apply_force {
                 let force = Force::linear({
                     (match &axis {
                         Axis::X => Vector::x(),
                         Axis::Y => Vector::y(),
-                    }) * dbg!(speed)
+                    }) * speed
                 });
 
                 rigid_body.apply_force(
@@ -107,40 +61,6 @@ fn handle_move_on_axis(
                     ForceType::AccelerationChange,
                     true,
                 );
-                // }
-
-                // Set to max velocity, if force was applied and is now above max velocity
-                // if should_apply_force && is_velocity_above_max(rigid_body) {
-                //     let velocity = {
-                //         let vel = rigid_body.velocity().as_vector();
-                //         (vel.x, vel.y)
-                //     };
-                //     match &axis {
-                //         Axis::X => {
-                //             if let Some(max_x) = physics_data.max_velocity.0 {
-                //                 if velocity.0 > max_x {
-                //                     rigid_body.set_velocity(Velocity::linear(
-                //                         max_x, velocity.1,
-                //                     ));
-                //                     rigid_body.clear_forces();
-                //                 }
-                //             }
-                //         }
-
-                //         Axis::Y => {
-                //             if let Some(max_y) = physics_data.max_velocity.1 {
-                //                 if velocity.1 > max_y {
-                //                     rigid_body.set_velocity(Velocity::linear(
-                //                         velocity.0, max_y,
-                //                     ));
-                //                     rigid_body.clear_forces();
-                //                 }
-                //             }
-                //         }
-                //     }
-                // }
-
-                dbg!(rigid_body.velocity());
             }
         }
     }
