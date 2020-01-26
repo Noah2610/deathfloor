3a770f48627afeea53b2085eba97b2b578a33282
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-01-26 22:42:13 +0100

    Add 64x64-tile-sized map for debugging

---
+++ tiled/tileset64.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tileset64" tilewidth="64" tileheight="64" tilecount="2" columns="2">
+ <image source="../resources/spritesheets/tiles/temp_tileset1.png" width="128" height="64"/>
+ <tile id="0" type="Ground"/>
+</tileset>
