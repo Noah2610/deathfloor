94a7a385cf67435e658b53f1214415473dfb8b22
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-19 18:41:03 +0100

    Add collision rectangles to dirt.tsx tileset
    Export script doesn't do anything with it yet, just playing around

--- tiled/tsx/dirt.tsx
+++ tiled/tsx/dirt.tsx
@@ -1,9 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="dirt" tilewidth="16" tileheight="16" tilecount="81" columns="9">
+<tileset version="1.2" tiledversion="1.3.1" name="dirt" tilewidth="16" tileheight="16" tilecount="81" columns="9">
  <image source="../../docs/concept-art/themes/DFCA1.png" width="144" height="144"/>
  <tile id="13" type="Ground"/>
  <tile id="14" type="Ground"/>
- <tile id="20" type="SGround"/>
+ <tile id="20" type="SGround">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.02029" y="1.91304" width="16.1072" height="11.087"/>
+  </objectgroup>
+ </tile>
  <tile id="48" type="WeirdGround"/>
  <tile id="49" type="WeirdGround"/>
 </tileset>
