175b75f357e37fa8fc745b9abe41ee9e5c3e58b8
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-03-11 00:25:49 +0100

    Remove unnecessary TryFrom<&str> impls for TileType and ObjectType

--- src/map_loader/objects/object_type.rs
+++ src/map_loader/objects/object_type.rs
@@ -1,27 +1,6 @@
-use deathframe::amethyst;
-use std::convert::TryFrom;
-
 #[derive(Debug, Deserialize, Serialize)]
 pub enum ObjectType {
     #[serde(rename = "")]
     None,
     Player,
-}
-
-impl TryFrom<&str> for ObjectType {
-    type Error = amethyst::Error;
-    fn try_from(s: &str) -> amethyst::Result<Self> {
-        // In JSON, this would be a string, so the input string needs to be wrapped in "".
-        let s = format!("\"{}\"", s);
-        serde_json::from_str::<Self>(&s).map_err(|_| {
-            amethyst::Error::from_string(format!("Invalid object type: {}", s))
-        })
-    }
-}
-
-impl TryFrom<String> for ObjectType {
-    type Error = amethyst::Error;
-    fn try_from(s: String) -> amethyst::Result<Self> {
-        Self::try_from(s.as_str())
-    }
 }
--- src/map_loader/tiles/tile_type.rs
+++ src/map_loader/tiles/tile_type.rs
@@ -1,6 +1,3 @@
-use deathframe::amethyst;
-use std::convert::TryFrom;
-
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
 pub enum TileType {
     #[serde(rename = "")]
@@ -11,23 +8,5 @@ pub enum TileType {
 impl Default for TileType {
     fn default() -> Self {
         TileType::None
-    }
-}
-
-impl TryFrom<&str> for TileType {
-    type Error = amethyst::Error;
-    fn try_from(s: &str) -> amethyst::Result<Self> {
-        // In JSON, this would be a string, so the input string needs to be wrapped in "".
-        let s = format!("\"{}\"", s);
-        serde_json::from_str::<Self>(&s).map_err(|_| {
-            amethyst::Error::from_string(format!("Invalid tile type: {}", s))
-        })
-    }
-}
-
-impl TryFrom<String> for TileType {
-    type Error = amethyst::Error;
-    fn try_from(s: String) -> amethyst::Result<Self> {
-        Self::try_from(s.as_str())
     }
 }
