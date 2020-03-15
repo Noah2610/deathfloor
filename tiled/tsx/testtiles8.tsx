7969c84835726c89f89d78fb56a68772e97022a9
Emu <emurenn@gmail.com>
2020-03-15 13:51:48 +0100

    New tileset (png + tsx) (work in progress)

---
+++ tiled/tsx/testtiles8.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="testtiles8" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="../../resources/spritesheets/tiles/testtiles8.png" width="256" height="256"/>
+ <tile id="81" type="Jumppad">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="4" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="82" type="Jumppad">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8"/>
+   <object id="2" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="83" type="Jumppad">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="84" type="Jumppad">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="4" height="8"/>
+  </objectgroup>
+ </tile>
+</tileset>
