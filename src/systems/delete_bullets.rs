7c46b6d5187e1d7cbc9a4c81d05d1ca7a6bdb7f2
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-03-20 20:06:21 +0100

    Write DeleteBulletsSystem
    Issue #9

--- src/systems/delete_bullets.rs
+++ src/systems/delete_bullets.rs
@@ -4,8 +4,19 @@ use super::system_prelude::*;
 pub struct DeleteBulletsSystem;
 
 impl<'a> System<'a> for DeleteBulletsSystem {
-    type SystemData = ();
+    type SystemData = (Entities<'a>, WriteStorage<'a, Bullet>);
 
-    fn run(&mut self, (): Self::SystemData) {
+    fn run(&mut self, (entities, mut bullets): Self::SystemData) {
+        for (entity, bullet) in (&entities, &mut bullets).join() {
+            if bullet.despawn_timer.state.is_stopped() {
+                bullet.despawn_timer.start().unwrap();
+            }
+            bullet.despawn_timer.update().unwrap();
+            if bullet.despawn_timer.state.is_finished() {
+                entities
+                    .delete(entity)
+                    .expect("Couldn't delete bullet entity");
+            }
+        }
     }
 }
