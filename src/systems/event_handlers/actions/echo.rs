eef6199a9b76a44d5a284bb31554a9e166cd2455
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-03-24 17:03:23 +0100

    HandleActionEcho's SystemData is only EventListener storage, no tuple
    Issue #10

--- src/systems/event_handlers/actions/echo.rs
+++ src/systems/event_handlers/actions/echo.rs
@@ -4,11 +4,10 @@ use super::system_prelude::*;
 pub struct HandleActionEcho;
 
 impl<'a> System<'a> for HandleActionEcho {
-    type SystemData = (Entities<'a>, WriteStorage<'a, EventListener>);
+    type SystemData = WriteStorage<'a, EventListener>;
 
-    fn run(&mut self, (entities, mut event_listener_store): Self::SystemData) {
-        for (_, event_listener) in (&entities, &mut event_listener_store).join()
-        {
+    fn run(&mut self, mut event_listener_store: Self::SystemData) {
+        for event_listener in (&mut event_listener_store).join() {
             if let Some(actions) =
                 event_listener.take_actions(&EventActionType::Echo)
             {
