f85706439753c50707b7197e50eaa9e0f43f1c52
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-03-30 21:37:16 +0200

    Load enemy configs from files in settings/enemies/ directory (recursively)
    Load all files into `EnemiesSettings`,
    then merge them all together.

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -16,8 +16,12 @@ mod hitbox_config;
 mod player_settings;
 mod tiles_settings;
 
+use crate::helpers::resource;
 use deathframe::amethyst;
 use prelude::*;
+use std::collections::HashMap;
+use std::fs::File;
+use std::path::PathBuf;
 
 #[derive(Clone, Deserialize)]
 pub struct Settings {
@@ -35,7 +39,7 @@ impl Settings {
             camera:  Self::load_file::<CameraSettings, _>("camera.ron")?,
             player:  Self::load_file::<PlayerSettings, _>("player.ron")?,
             tiles:   Self::load_file::<TilesSettings, _>("tiles.ron")?,
-            enemies: Self::load_file::<EnemiesSettings, _>("enemies.ron")?,
+            enemies: Self::load_enemies_config()?,
         })
     }
 
@@ -44,9 +48,6 @@ impl Settings {
         for<'de> T: serde::Deserialize<'de>,
         S: std::fmt::Display,
     {
-        use crate::helpers::resource;
-        use std::fs::File;
-
         let file = File::open(resource(format!("settings/{}", filename)))?;
         Ok(ron::de::from_reader(file).map_err(|e| {
             amethyst::Error::from_string(format!(
@@ -54,6 +55,54 @@ impl Settings {
                 filename, e
             ))
         })?)
+    }
+
+    fn load_enemies_config() -> amethyst::Result<EnemiesSettings> {
+        let path = resource("settings/enemies");
+        let all_enemies_settings = Self::load_enemies_configs_from(path)?;
+        let enemies_settings =
+            Self::merge_enemies_settings(all_enemies_settings);
+        Ok(enemies_settings)
+    }
+
+    fn load_enemies_configs_from(
+        path: PathBuf,
+    ) -> amethyst::Result<Vec<EnemiesSettings>> {
+        let mut settings = Vec::new();
+
+        for entry in path.read_dir()? {
+            let entry_path = entry?.path();
+            if entry_path.is_file() {
+                if let Some("ron") =
+                    entry_path.extension().and_then(|e| e.to_str())
+                {
+                    let file = File::open(&entry_path)?;
+                    settings.push(ron::de::from_reader(file).map_err(|e| {
+                        amethyst::Error::from_string(format!(
+                            "Failed parsing ron settings file: {:?}\n{:#?}",
+                            entry_path, e
+                        ))
+                    })?);
+                }
+            } else if entry_path.is_dir() {
+                settings
+                    .append(&mut Self::load_enemies_configs_from(entry_path)?);
+            }
+        }
+
+        Ok(settings)
+    }
+
+    fn merge_enemies_settings(
+        all_enemies_settings: Vec<EnemiesSettings>,
+    ) -> EnemiesSettings {
+        let mut enemies_types = HashMap::new();
+        for enemies_settings in all_enemies_settings {
+            enemies_types.extend(enemies_settings.types);
+        }
+        EnemiesSettings {
+            types: enemies_types,
+        }
     }
 }
 
