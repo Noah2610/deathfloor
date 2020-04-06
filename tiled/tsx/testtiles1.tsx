dff36dc77ae89914526e33e47846ace84e80cf35
emu <31223078+hoichael@users.noreply.github.com>
2020-04-06 06:49:12 +0200

    Changes to testlevel1 and testtiles1

--- tiled/tsx/testtiles1.tsx
+++ tiled/tsx/testtiles1.tsx
@@ -1,57 +1,57 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="testtiles1" tilewidth="8" tileheight="8" tilecount="324" columns="18">
+<tileset version="1.2" tiledversion="1.3.3" name="testtiles1" tilewidth="8" tileheight="8" tilecount="324" columns="18">
  <image source="../../resources/spritesheets/tiles/testtiles1.png" width="144" height="144"/>
- <tile id="0">
+ <tile id="0" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="1">
+ <tile id="1" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="2">
+ <tile id="2" type="Ground">
   <objectgroup draworder="index" id="3">
    <object id="2" x="0" y="0" width="8" height="8"/>
    <object id="3" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="3">
+ <tile id="3" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="2" width="8" height="5.875"/>
    <object id="2" x="0" y="0" width="6" height="2"/>
    <object id="3" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="4">
+ <tile id="4" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="2" width="8" height="6"/>
    <object id="2" x="2" y="0" width="6" height="2"/>
   </objectgroup>
  </tile>
- <tile id="5">
+ <tile id="5" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
    <object id="2" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="6">
+ <tile id="6" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="7">
+ <tile id="7" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="12">
+ <tile id="12" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="6" height="8"/>
   </objectgroup>
  </tile>
- <tile id="13">
+ <tile id="13" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
@@ -62,13 +62,13 @@
    <object id="2" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="21">
+ <tile id="21" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
    <object id="2" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="22">
+ <tile id="22" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
@@ -88,17 +88,17 @@
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="30">
+ <tile id="30" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="6" height="8"/>
   </objectgroup>
  </tile>
- <tile id="31">
+ <tile id="31" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="72">
+ <tile id="72" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
@@ -113,7 +113,7 @@
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="75">
+ <tile id="75" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
@@ -123,7 +123,7 @@
    <object id="1" x="8" y="0"/>
   </objectgroup>
  </tile>
- <tile id="90">
+ <tile id="90" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
@@ -138,7 +138,7 @@
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="93">
+ <tile id="93" type="Ground">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
@@ -213,4 +213,8 @@
    <object id="1" x="0" y="0"/>
   </objectgroup>
  </tile>
+ <tile id="252" type="Jumppad"/>
+ <tile id="253" type="Jumppad"/>
+ <tile id="254" type="Jumppad"/>
+ <tile id="255" type="Jumppad"/>
 </tileset>
