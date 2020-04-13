957e2e20fb734a840c7ec4eb991f6499d2fe2dd5
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-13 21:01:34 +0200

    Setup MainMenu state, setup MenuBindings

---
+++ src/states/main_menu.rs
@@ -0,0 +1,77 @@
+use super::state_prelude::*;
+use crate::helpers::resource;
+use amethyst::ui::UiEvent;
+use deathframe::core::menu::prelude::*;
+
+#[derive(Default)]
+pub struct MainMenu {
+    ui_data: UiData,
+}
+
+impl MainMenu {
+    fn start<'a, 'b>(&mut self, data: &mut StateData<GameData<'a, 'b>>) {
+        self.create_ui(data, resource("ui/main_menu.ron").to_str().unwrap());
+    }
+
+    fn stop<'a, 'b>(&mut self, data: &mut StateData<GameData<'a, 'b>>) {
+        self.delete_ui(data);
+    }
+}
+
+impl<'a, 'b> Menu<GameData<'a, 'b>, StateEvent> for MainMenu {
+    fn event_triggered(
+        &mut self,
+        data: &mut StateData<GameData<'a, 'b>>,
+        event_name: String,
+        event: UiEvent,
+    ) -> Option<Trans<GameData<'a, 'b>, StateEvent>> {
+        None
+    }
+
+    fn ui_data(&self) -> &UiData {
+        &self.ui_data
+    }
+
+    fn ui_data_mut(&mut self) -> &mut UiData {
+        &mut self.ui_data
+    }
+}
+
+impl<'a, 'b> State<GameData<'a, 'b>, StateEvent> for MainMenu {
+    fn on_start(&mut self, mut data: StateData<GameData<'a, 'b>>) {
+        self.start(&mut data);
+    }
+
+    fn on_resume(&mut self, mut data: StateData<GameData<'a, 'b>>) {
+        self.start(&mut data);
+    }
+
+    fn on_stop(&mut self, mut data: StateData<GameData<'a, 'b>>) {
+        self.stop(&mut data);
+    }
+
+    fn on_pause(&mut self, mut data: StateData<GameData<'a, 'b>>) {
+        self.stop(&mut data);
+    }
+
+    fn update(
+        &mut self,
+        data: StateData<GameData<'a, 'b>>,
+    ) -> Trans<GameData<'a, 'b>, StateEvent> {
+        data.data
+            .update(data.world, DispatcherId::MainMenu)
+            .unwrap();
+        Trans::None
+    }
+
+    fn fixed_update(
+        &mut self,
+        mut data: StateData<GameData<'a, 'b>>,
+    ) -> Trans<GameData<'a, 'b>, StateEvent> {
+        if let Some(trans) = self.update_ui_events(&mut data) {
+            trans
+        } else {
+            Trans::None
+        }
+    }
+}
