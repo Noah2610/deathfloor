c3d1dcdab55ae7b3920c31715214ba2b171383e1
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-16 07:47:32 +0200

    Update amethyst to v0.15.0 via deathframe

--- src/init.rs
+++ src/init.rs
@@ -96,14 +96,14 @@ fn build_game_data<'a, 'b>(
         .with_core_bundle(transform_bundle)?
         .with_core_bundle(rendering_bundle)?
         .with_core_bundle(audio_bundle)?
+        .with_core_bundle(menu_input_bundle)?
         .with_core_bundle(UiBundle::<input::MenuBindings>::new())?
         .with_core(ScaleSpritesSystem::default(), "scale_sprites_system", &[])?
         .with_core(CameraOrthoSystem::default(), "camera_ortho_system", &[])?
         .with_bundle(DispatcherId::Ingame, ingame_input_bundle)?
-        .with_bundle(DispatcherId::Paused, paused_input_bundle)?
-        .with_bundle(DispatcherId::MainMenu, menu_input_bundle)?
         .with_bundle(DispatcherId::Ingame, physics_bundle)?
         .with_bundle(DispatcherId::Ingame, EventHandlersBundle::default())?
+        .with_bundle(DispatcherId::Paused, paused_input_bundle)?
         .with(
             DispatcherId::Ingame,
             HandleAnimationsSystem::default(),
