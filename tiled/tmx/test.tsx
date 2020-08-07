a7d2491b5889e8a800846010013a33e19aea22f3
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-08-08 00:07:00 +0200

    Add Shooter enemy and ShooterBullet entity configs

---
+++ tiled/tmx/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="test" tilewidth="8" tileheight="8" tilecount="144" columns="24">
+ <image source="../../resources/spritesheets/enemy_shooter.png" width="192" height="48"/>
+</tileset>
