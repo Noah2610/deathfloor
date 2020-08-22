f9c4f928dd3e9832a113c55da26dbff1459be2bd
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-08-22 13:31:39 +0200

    Add system helper add_entity_config
    Refactored from map_loader helper `edit_entity_with_entity_config`,
    so it can be used without world.

--- src/systems/system_helpers/mod.rs
+++ src/systems/system_helpers/mod.rs
@@ -1,7 +1,9 @@
 pub mod prelude {
+    pub use super::add_entity_config::add_entity_config;
     pub use super::insert_components::insert_components;
 }
 
+mod add_entity_config;
 mod insert_components;
 
 use super::system_prelude;
