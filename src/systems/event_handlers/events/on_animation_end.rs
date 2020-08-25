bd4125ac41175c009dc7820bae051297e3aa95bc
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-08-25 15:08:32 +0200

    Fix OnAnimationEnd event from triggering multiple times
    Use new `deathframe` `AnimationsContainer::last_finished_animation`
    method for checking if animation finished playing.
    
    Issue #57

--- src/systems/event_handlers/events/on_animation_end.rs
+++ src/systems/event_handlers/events/on_animation_end.rs
@@ -1,13 +1,16 @@
 use super::system_prelude::*;
+use std::collections::HashMap;
 
 #[derive(Default)]
-pub struct HandleEventOnAnimationEnd;
+pub struct HandleEventOnAnimationEnd {
+    last_finished_anims: HashMap<Entity, AnimationKey>,
+}
 
 impl<'a> System<'a> for HandleEventOnAnimationEnd {
     type SystemData = (
+        Entities<'a>,
         ReadStorage<'a, EventsRegister>,
         WriteStorage<'a, ActionTypeTrigger>,
-        ReadStorage<'a, Animation>,
         ReadStorage<'a, AnimationsContainer<AnimationKey>>,
         ReadStorage<'a, Loadable>,
         ReadStorage<'a, Loaded>,
@@ -16,25 +19,25 @@ impl<'a> System<'a> for HandleEventOnAnimationEnd {
     fn run(
         &mut self,
         (
+            entities,
             events_register_store,
             mut action_type_trigger_store,
-            animation_store,
             animations_container_store,
             loadable_store,
             loaded_store,
         ): Self::SystemData,
     ) {
         for (
+            entity,
             events_register,
             action_type_trigger,
-            animation,
             animations,
             loadable_opt,
             loaded_opt,
         ) in (
+            &entities,
             &events_register_store,
             &mut action_type_trigger_store,
-            &animation_store,
             &animations_container_store,
             loadable_store.maybe(),
             loaded_store.maybe(),
@@ -46,12 +49,21 @@ impl<'a> System<'a> for HandleEventOnAnimationEnd {
             {
                 for (event, action) in events_register.events().iter() {
                     if let EventType::OnAnimationEnd(target_anim) = event {
-                        if let Some(current) = animations.current() {
-                            if current == target_anim {
-                                if animation.has_played_and_is_finished() {
-                                    action_type_trigger
-                                        .add_action(action.clone());
-                                }
+                        if let Some(last_anim) =
+                            animations.last_finished_animation()
+                        {
+                            if last_anim == target_anim
+                                && self
+                                    .last_finished_anims
+                                    .get(&entity)
+                                    .map(|saved_last_anim| {
+                                        saved_last_anim != last_anim
+                                    })
+                                    .unwrap_or(true)
+                            {
+                                action_type_trigger.add_action(action.clone());
+                                self.last_finished_anims
+                                    .insert(entity, last_anim.clone());
                             }
                         }
                     }
