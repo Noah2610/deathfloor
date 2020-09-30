855d0526ff7131f5707073f2a7b17867d0758725
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-09-30 05:37:05 +0200

    Add Jump, Jumping, InAir animation keys
    Rework `HandleAnimationsSystem` to include these animations.
    Configure player jump animations.
    
    Issue #87

--- src/animation_key.rs
+++ src/animation_key.rs
@@ -2,10 +2,49 @@ use std::fmt;
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, Deserialize)]
 pub enum AnimationKey {
+    /// Played when standing on the ground and not moving.
+    ///
+    /// `Cycle` animation.
     Idle,
+
+    /// Played when standing on the ground and moving left/right.
+    ///
+    /// `Cycle` animation.
     Walk,
+
+    /// Played once while dying (lifecycle state `Death`).
+    ///
+    /// `Once` animation.
     Death,
+
+    /// Played once on jump.
+    ///
+    /// `Once` animation.
+    Jump,
+
+    /// Plays after jump and after `Jump` animation played,
+    /// while jumping (while holding down the jump button),
+    /// while in-air, and while moving upwards (positive y velocity).
+    ///
+    /// `Cycle` animation.
+    Jumping,
+
+    /// Played when in the air and not jumping.
+    ///
+    /// `Cycle` animation.
+    InAir,
+
+    /// Custom animations, can be played with `AnimationAction`.
     Custom(String),
+}
+
+impl AnimationKey {
+    pub fn is_custom(&self) -> bool {
+        match self {
+            Self::Custom(_) => true,
+            _ => false,
+        }
+    }
 }
 
 // TODO
@@ -23,6 +62,9 @@ impl fmt::Display for AnimationKey {
             Self::Idle => "Idle".to_string(),
             Self::Walk => "Walk".to_string(),
             Self::Death => "Death".to_string(),
+            Self::Jump => "Jump".to_string(),
+            Self::Jumping => "Jumping".to_string(),
+            Self::InAir => "InAir".to_string(),
             Self::Custom(s) => format!("Custom({})", s),
         })
     }
--- src/systems/handle_animations.rs
+++ src/systems/handle_animations.rs
@@ -1,6 +1,7 @@
 use super::system_prelude::*;
 
-const PADDING: f32 = 8.0;
+const WALK_ANIM_PADDING: f32 = 8.0;
+const JUMPING_ANIM_PADDING: f32 = 0.0;
 
 #[derive(Default)]
 pub struct HandleAnimationsSystem;
@@ -8,24 +9,90 @@ pub struct HandleAnimationsSystem;
 impl<'a> System<'a> for HandleAnimationsSystem {
     type SystemData = (
         Entities<'a>,
-        ReadStorage<'a, Velocity>,
         WriteStorage<'a, AnimationsContainer<AnimationKey>>,
+        ReadStorage<'a, Velocity>,
+        ReadStorage<'a, Collider<CollisionTag>>,
+        ReadStorage<'a, Jumper>,
     );
 
     fn run(
         &mut self,
-        (entities, velocities, mut animation_containers): Self::SystemData,
+        (
+            entities,
+            mut animations_store,
+            velocity_store,
+            collider_store,
+            jumper_store,
+        ): Self::SystemData,
     ) {
-        for (_, velocity, animations) in
-            (&entities, &velocities, &mut animation_containers).join()
+        let on_ground_query = {
+            use deathframe::physics::query::exp::prelude_variants::*;
+            let solid_tag = CollisionTag::from(CollisionLabel::solid());
+            And(vec![IsTag(solid_tag), IsSide(Bottom)])
+        };
+
+        for (_, animations, velocity, collider_opt, jumper_opt) in (
+            &entities,
+            &mut animations_store,
+            &velocity_store,
+            collider_store.maybe(),
+            jumper_store.maybe(),
+        )
+            .join()
         {
-            // TODO
-            // handle other axis
-            let _ = match velocity.x {
-                x if x > PADDING => animations.play(AnimationKey::Walk),
-                x if x < -PADDING => animations.play(AnimationKey::Walk),
-                _ => animations.play(AnimationKey::Idle),
-            };
+            // Only play animation if there is no `Custom` animation playing.
+            // `Custom` animations take precedence.
+            let should_play_anim = animations
+                .current()
+                .map(|anim| !anim.is_custom())
+                .unwrap_or(true);
+
+            if should_play_anim {
+                let (did_jump, is_jumping) = jumper_opt
+                    .map(|jumper| (jumper.did_jump, jumper.is_jumping))
+                    .unwrap_or((false, false));
+                let is_in_air = collider_opt
+                    .map(|collider| {
+                        collider
+                            .query::<FindQuery<_>>()
+                            .exp(&on_ground_query)
+                            .run()
+                            .is_none()
+                    })
+                    .unwrap_or(false);
+
+                let anim_action = if did_jump
+                    && animations.has_animation(&AnimationKey::Jump)
+                // JUMP ANIM
+                {
+                    AnimationAction::Push(AnimationKey::Jump)
+                } else if is_jumping
+                    && is_in_air
+                    && velocity.y > JUMPING_ANIM_PADDING
+                    && animations.has_animation(&AnimationKey::Jumping)
+                // JUMPING ANIM
+                {
+                    AnimationAction::Play(AnimationKey::Jumping)
+                } else if is_in_air
+                    && animations.has_animation(&AnimationKey::InAir)
+                // IN-AIR ANIM
+                {
+                    AnimationAction::Play(AnimationKey::InAir)
+                } else {
+                    // WALK / IDLE ANIM
+                    AnimationAction::Play(match velocity.x {
+                        x if x > WALK_ANIM_PADDING => AnimationKey::Walk,
+                        x if x < -WALK_ANIM_PADDING => AnimationKey::Walk,
+                        _ => AnimationKey::Idle,
+                    })
+                };
+
+                let _ = match anim_action {
+                    AnimationAction::Play(anim) => animations.play(anim),
+                    AnimationAction::Push(anim) => animations.push(anim),
+                    AnimationAction::Pop => animations.pop().map(|_| ()),
+                };
+            }
         }
     }
 }
