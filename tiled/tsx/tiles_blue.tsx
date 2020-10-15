e9fca0a1d0fecb42eae74c43340f585351009c88
emu <31223078+hoichael@users.noreply.github.com>
2020-10-15 16:37:17 +0200

    Moved some stuff + misc changes

---
+++ tiled/tsx/tiles_blue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tiles_blue" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
+ <image source="../../resources/spritesheets/tiles/final/blue/tiles_blue.png" width="512" height="512"/>
+</tileset>
