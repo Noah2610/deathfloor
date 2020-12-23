2bd6749f77f4d960b2bfee815de43f111ee12eff
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-12-23 12:15:31 +0100

    Use ExpressionOperations in If Conditions

--- src/components/events_register/actions/conditionals/condition.rs
+++ src/components/events_register/actions/conditionals/condition.rs
@@ -2,40 +2,11 @@ use crate::expression::{Expression, ExpressionStorages};
 use deathframe::amethyst::ecs::Entity;
 
 /// A _condition_ used with conditional actions (such as `If`).
-/// A condition returns `true` or `false`.
+/// Is simply a wrapper for an `Expression`, which when used
+/// as a conditional is converted to a boolean value (truthy/falsy).
 #[derive(Deserialize, Clone)]
-pub enum Condition {
-    /// Passes if both values are _equal_ (`==`).
-    #[serde(alias = "Eq")]
-    Equal(Expression, Expression),
-
-    /// Passes if the first value is _less than_ the second value (`<`).
-    #[serde(alias = "Lt")]
-    LessThan(Expression, Expression),
-
-    /// Passes if the first value is _greater than_ the second value (`>`).
-    #[serde(alias = "Gt")]
-    GreaterThan(Expression, Expression),
-
-    /// Always passes (returns `true`).
-    #[serde(alias = "true")]
-    True,
-
-    /// Never passes (returns `false`).
-    #[serde(alias = "false")]
-    False,
-
-    /// Inverts the given condition.
-    Not(Box<Condition>),
-
-    /// `And`s the given conditions together.
-    /// All given conditions must pass for this condition to pass.
-    And(Vec<Condition>),
-
-    /// `Or`s the given conditions together.
-    /// Any of the given conditions must pass for this condition to pass.
-    Or(Vec<Condition>),
-}
+#[serde(deny_unknown_fields, from = "Expression")]
+pub struct Condition(Expression);
 
 impl Condition {
     pub fn passes(
@@ -43,26 +14,12 @@ impl Condition {
         entity: Entity,
         storages: &ExpressionStorages,
     ) -> bool {
-        use Condition::*;
-        match self {
-            Equal(one, two) => {
-                one.get(entity, storages) == two.get(entity, storages)
-            }
-            LessThan(one, two) => {
-                one.get(entity, storages) < two.get(entity, storages)
-            }
-            GreaterThan(one, two) => {
-                one.get(entity, storages) > two.get(entity, storages)
-            }
-            True => true,
-            False => false,
-            Not(condition) => !condition.passes(entity, storages),
-            And(conditions) => conditions
-                .iter()
-                .all(|condition| condition.passes(entity, storages)),
-            Or(conditions) => conditions
-                .iter()
-                .any(|condition| condition.passes(entity, storages)),
-        }
+        self.0.get(entity, storages).is_truthy()
+    }
+}
+
+impl From<Expression> for Condition {
+    fn from(exp: Expression) -> Self {
+        Self(exp)
     }
 }
